7a584f93d687c9b03db6397dc02e2d5ce26164ce
Abel Ratanaphan <abelrat@yahoo.com>
2024-11-04 22:36:50 -0500

    feat: PythonFlask backend and React frontend complete

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,17 +1,18 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
+# Default route
+@app.route('/')
+def home():
+    return "Welcome to the Flask App Backend!"
 
 # Members API route
 @app.route('/members')
 def members():
     return {'members': ['Member1', 'Member2', 'Member3']}
 
-# Catch-all 404 error handler
-@app.errorhandler(404)
-def not_found(error):
-    return jsonify({'error': 'This route does not exist'}), 404
-
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    app.run(port=5001, debug=True)
